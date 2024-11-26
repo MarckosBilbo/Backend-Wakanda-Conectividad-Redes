2e9a87be6b898f8693d1e90994f412150457e763
marcos <marcgabe@myuax.com>
2024-11-26 13:46:42 +0100

    Initial commit

---
+++ src/main/java/org/example/backendwakandaconectividadredes/BackendWakandaConectividadRedesApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaconectividadredes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaConectividadRedesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWakandaConectividadRedesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backendwakandaconectividadredes/BackendWakandaConectividadRedesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaconectividadredes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaConectividadRedesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
