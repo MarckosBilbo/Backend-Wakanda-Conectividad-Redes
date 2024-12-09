716b2df1642a5bfb89a65c2327bccd883dff2bd4
Jmrodrival04 <jvaldrod@myuax.com>
2024-12-09 18:41:29 +0100

    --

---
+++ src/main/java/org/example/backendwakandaconectividadredes/repos/ProveedorInternetRepository.java
@@ -0,0 +1,14 @@
+package org.example.backendwakandaconectividadredes.repos;
+
+import org.example.backendwakandaconectividadredes.model.ProveedorInternet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProveedorInternetRepository extends JpaRepository<ProveedorInternet, Long> {
+
+    // Buscar proveedores por tipo de servicio
+    List<ProveedorInternet> findByTipoDeServicio(String tipoDeServicio);
+}
